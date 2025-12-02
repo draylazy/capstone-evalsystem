c5d0e11dd8a077bed378a9420f6419fa5c01dc7f
Nico <rivera.nicon2020@gmail.com>
2025-12-03 01:14:10 +0800

    Created folders in frontend and added Login.js and css, and change application properties for db

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -6,9 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
